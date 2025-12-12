7100d32dc3554bbe20861da997998f8970c8fc86
vanya-vb <vbozadzhieva29@gmail.com>
2025-12-12 12:36:56 +0200

    setup backed with convex

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,20 @@
 import { ThemeProvider } from "@/hooks/useTheme";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
-      </Stack>
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ title: "Home" }} />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
   )
 }
